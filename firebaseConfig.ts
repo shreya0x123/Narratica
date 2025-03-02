bdf7ab0799bdfda99c7e1efeeb26695525ab9e1d
shreya0x123 <shreyakamath.04@gmail.com>
2025-03-02 19:01:12 +0530

    Update firebaseConfig.ts

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -8,17 +8,7 @@ import {
 
 // ✅ Your Firebase Config
 const firebaseConfig = {
-  apiKey: "AIzaSyAaw4aTWlEncshnWJ2rCWif_wif4SiS3Ic",
-
-  authDomain: "narratica-e30a9.firebaseapp.com",
-
-  projectId: "narratica-e30a9",
-
-  storageBucket: "narratica-e30a9.firebasestorage.app",
-
-  messagingSenderId: "390252562121",
-
-  appId: "1:390252562121:web:6837dce2d9747bf3a9af18",
+ 
 };
 
 // ✅ Initialize Firebase
